50486d2077a56ef8235f18ccc889643c4ec7c607
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-06-26 04:29:27 +0600

    Implemented core parser functions with initial testing
    Added implementations for:
    
    * `parse()`
    * `parse_statement()`
    * `parse_var_declaration()`
    * `parse_expression()`
    * `parse_call_args()`
    
    Included initial testing through `main()` to verify parser behavior.
    
    Remaining parser functions:
    
    * `parse_binary_expression()`
    * `parse_if_statement()`
    * `parse_while_statement()`
    * `parse_function_declaration()`
    * `parse_return_statement()`

--- parser.py
+++ parser.py
@@ -18,6 +18,89 @@ class Parser:
             return None
         return self.tokens[self.index+1]
 
+    def parse(self):
+        statements = []
+
+        while self.current_token.type != "EOF":
+            # skip new lines
+            if self.current_token.type == "NEWLINE":
+                self.advance()
+            else:
+                statement = self.parse_statement()
+                statements.append(statement)
+        
+        return Program(statements)
+    
+    def parse_statement(self):
+        if self.current_token.type in ("LET", "CONST"):
+            return self.parse_var_declaration()
+        elif self.current_token.type == "IF":
+            return self.parse_if_statement()
+        elif self.current_token.type == "WHILE":
+            return self.parse_while_statement()
+        elif self.current_token.type == "FUNCTION":
+            return self.parse_function_declaration()
+        elif self.current_token.type == "RETURN":
+            return self.parse_return_statement()
+        elif self.current_token.type in ("PRINT", "PRINT_LOUD", "INPUT"):
+            return self.parse_expression()
+        elif self.current_token.type == "IDENT":
+            return self.parse_expression()
+        else:
+            raise Exception(f"my guy.. never seen '{self.current_token.type}' in my life btw")
+
+    def parse_var_declaration(self):
+        constant = self.current_token.type == "CONST" # is constant? if yes, return true - else, false
+        self.advance()
+        name = self.current_token.value
+        self.advance()
+        self.advance() # skip ASSIGN
+        value = self.parse_expression() 
+        return VarDeclaration(name, value, constant)
+    
+    def parse_expression(self):
+        if self.current_token.type == "NUMBER":
+            node = NumberLiteral(self.current_token.value)
+            self.advance()
+            return node
+        elif self.current_token.type == "STRING":
+            node = StringLiteral(self.current_token.value)
+            self.advance()
+            return node
+        elif self.current_token.type in ("TRUE", "FALSE"):
+            node = BoolLiteral(self.current_token.value)
+            self.advance()
+            return node
+        elif self.current_token.type == "IDENT":
+            name = self.current_token.value
+            self.advance()
+
+            if self.current_token.type is not None and self.current_token.type == "LPAREN":
+                args = self.parse_call_args()
+                return CallExpression(name, args)
+            else:
+                return Identifier(name)
+        elif self.current_token.type in ("PRINT", "PRINT_LOUD", "INPUT"):
+            name = self.current_token.value  
+            self.advance()
+            
+            if self.current_token.type is not None and self.current_token.type == "LPAREN":
+                args = self.parse_call_args()
+                return CallExpression(name, args)
+        else:
+            raise Exception(f"i dont know how you expect me to parse expression {self.current_token.value}")
+    
+    def parse_call_args(self):
+        self.advance()
+        args = []
+        while self.current_token.type is not None and self.current_token.type != "RPAREN":
+            if self.current_token.type == "COMMA":
+                self.advance()
+            args.append(self.parse_expression())
+        self.advance()
+        return args
+
+
 class VarDeclaration:
     def __init__(self, name, value, constant):
         self.name = name
@@ -99,3 +182,13 @@ class Program:
         self.statements = statements
     def __repr__(self):
         return f"Program(statements={self.statements})"
+
+if __name__ == "__main__":
+    from lexer import Lexer
+    
+    lexer = Lexer('yap("whats up pookie bear")')
+    tokens = lexer.tokenize()
+    
+    parser = Parser(tokens)
+    ast = parser.parse()
+    print(ast)
