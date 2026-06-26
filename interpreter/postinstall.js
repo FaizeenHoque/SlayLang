d2f903ee2aeb1f05d99b1ce00e03317741863337
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-06-26 22:49:44 +0600

    Added readme and other stuff

--- interpreter/postinstall.js
+++ interpreter/postinstall.js
@@ -1,11 +1,11 @@
 const { execSync } = require('child_process');
 const path = require('path');
 
-const vsix = path.join(__dirname, 'slay-lang-0.1.1.vsix');
+const vsix = path.join(__dirname, 'slay-lang-0.1.2.vsix');
 
 try {
     execSync(`code --install-extension ${vsix}`, { stdio: 'inherit' });
     console.log('SlayLang VS Code extension installed!');
 } catch (e) {
-    console.log('Could not install VS Code extension automatically. Install it manually from slay-lang-0.1.1.vsix');
+    console.log('Could not install VS Code extension automatically. Install it manually from slay-lang-0.1.2.vsix');
 }
